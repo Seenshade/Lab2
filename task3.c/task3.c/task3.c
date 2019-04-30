3445dcc026d53bf0025a4f263cc25c70bb2f66d2
Seenshade <Seenshade.sb@gmail.com>
2019-04-30 14:55:46 +0300

    Add files via upload

---
+++ task3.c/task3.c/task3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <Windows.h>
+
+void main(void) {
+	char a;
+	printf("Input a: ");
+	a = getch();
+	switch (a)
+	{
+	case '1':
+		
+		break;
+	case '2':
+		
+		break;
+	default:
+		printf("Wrong task.\n");
+		break;
+	}
+	system("pause");
+}
